951ffd5c8f03b57acd191ca03d6dd93f02e0e111
wadewithers <wit20007@byui.edu>
2023-10-21 08:39:59 -0600

    Login Page

--- App.js
+++ App.js
@@ -2,11 +2,12 @@ import * as React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, processColor } from 'react-native';
 import Create from "./src/frontend/create"
+import Login from "./src/frontend/login"
 
 
 export default function App() {
   return (
-    <Create/>
+    <Login/>
   );
 }
 
---
+++ src/frontend/login.js
@@ -0,0 +1,133 @@
+import React, { useState } from "react";
+import {TouchableOpacity, Pressable, TextInput, View, Text, Button} from 'react-native';
+import { Ionicons } from "@expo/vector-icons";
+
+
+const Login = ({ navigation }) => {
+    const [isPasswordShown, setIsPasswordShown] = useState(false);
+    const [email, onChangeEmail] = useState('');
+    const [password, onChangePassword] = useState('');
+
+    return (
+        <View style={{ flex: 1, marginHorizontal: 22 }}>
+                <View style={{ marginVertical: 22 }}>
+                    <Text style={{
+                        fontSize: 22,
+                        fontWeight: 'bold',
+                        marginVertical: 12,
+                        color: 'black'
+                    }}>
+                        Login
+                    </Text>
+                </View>
+
+                <View style={{ marginBottom: 12 }}>
+                    <Text style={{
+                        fontSize: 16,
+                        fontWeight: 400,
+                        marginVertical: 8
+                    }}>Email address</Text>
+
+                    <View style={{
+                        width: "100%",
+                        height: 48,
+                        borderColor: 'black',
+                        borderWidth: 1,
+                        borderRadius: 8,
+                        alignItems: "center",
+                        justifyContent: "center",
+                        paddingLeft: 22
+                    }}>
+                        <TextInput
+                            placeholder='Enter your email address'
+                            placeholderTextColor={'black'}
+                            keyboardType='email-address'
+                            onChangeText={onChangeEmail}
+                            style={{
+                                width: "100%"
+                            }}
+                        />
+                    </View>
+                </View>
+
+                <View style={{ marginBottom: 12 }}>
+                    <Text style={{
+                        fontSize: 16,
+                        fontWeight: 400,
+                        marginVertical: 8
+                    }}>Password</Text>
+
+                    <View style={{
+                        width: "100%",
+                        height: 48,
+                        borderColor: 'black',
+                        borderWidth: 1,
+                        borderRadius: 8,
+                        alignItems: "center",
+                        justifyContent: "center",
+                        paddingLeft: 22
+                    }}>
+                        <TextInput
+                            placeholder='Enter your password'
+                            placeholderTextColor={'black'}
+                            onChangeText={onChangePassword}
+                            secureTextEntry={isPasswordShown}
+                            style={{
+                                width: "100%"
+                            }}
+                        />
+
+                        <TouchableOpacity
+                            onPress={() => setIsPasswordShown(!isPasswordShown)}
+                            style={{
+                                position: "absolute",
+                                right: 12
+                            }}
+                        >
+                            {
+                                isPasswordShown == true ? (
+                                    <Ionicons name="eye-off" size={24} color={'black'} />
+                                ) : (
+                                    <Ionicons name="eye" size={24} color={'black'} />
+                                )
+                            }
+
+                        </TouchableOpacity>
+                    </View>
+                </View>
+                <View style={{
+                        width: "100%",
+                        height: 48,
+                        borderColor: 'black',
+                        borderWidth: 1,
+                        borderRadius: 8,
+                        alignItems: "center",
+                        justifyContent: "center",
+                        paddingLeft: 1
+                    }}>
+                    {/* Other signup-related components */}
+                    <Button title="Sign In" onPress={() => navigation.navigate("main")} />
+                </View>
+
+                <View style={{
+                    flexDirection: "row",
+                    justifyContent: "center",
+                    marginVertical: 22
+                }}>
+                    <Text style={{ fontSize: 16, color: 'black' }}>Don't have an account? </Text>
+                    <Pressable
+                        onPress={() => navigation.navigate("create")}
+                    >
+                        <Text style={{
+                            fontSize: 16,
+                            color: 'primary',
+                            fontWeight: "bold",
+                            marginLeft: 6
+                        }}>Sign Up</Text>
+                    </Pressable>
+                </View>
+            </View>
+    );
+};
+
+export default Login
